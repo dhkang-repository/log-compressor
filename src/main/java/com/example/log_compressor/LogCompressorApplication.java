9672915e58a176edea9cd0c22df99076cd0a8cf1
dhkangRepository <dhkang.repository@gmail.com>
2024-07-04 00:29:05 +0900

    feat : 스케쥴링 기능 추가

--- src/main/java/com/example/log_compressor/LogCompressorApplication.java
+++ src/main/java/com/example/log_compressor/LogCompressorApplication.java
@@ -4,10 +4,11 @@ import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,15 +18,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
 @SpringBootApplication
-public class LogCompressorApplication implements CommandLineRunner {
+@EnableScheduling
+public class LogCompressorApplication {
 
 	@Value("${log.path.default}")
 	private String defaultPath;
+
+	@Value("${log.filenames}")
+	private Set<String> fileSet;
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(LogCompressorApplication.class);
@@ -33,47 +37,37 @@ public class LogCompressorApplication implements CommandLineRunner {
 		app.run(args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello, I'm Log Compressor!");
-		if (args.length < 1) {
-			System.err.println("Usage: LogCompressorApp <log-directory>");
-			System.exit(1);
-		}
-
-		String logDirPath = args[0];
-		Path logDir = Paths.get(logDirPath);
-
-		if (!Files.exists(logDir) || !Files.isDirectory(logDir)) {
-			System.err.println("Invalid log directory: " + logDirPath);
-			System.exit(1);
-		}
-
-		LocalDate today = LocalDate.now();
+	@Scheduled(cron = "5 * * * * ?", zone = "Asia/Seoul") // 매일 오전 4시에 실행
+	private void scheduleLogCompression() throws IOException {
+		StringBuilder stringBuilder = new StringBuilder();
+		LocalDate today = LocalDate.now().minusDays(1L);
 		String dateSuffix = today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-		String tarGzFileName = "filename-" + dateSuffix + ".tar.gz";
 
-		try (FileOutputStream fos = new FileOutputStream(tarGzFileName);
-			 GzipCompressorOutputStream gcos = new GzipCompressorOutputStream(fos);
-			 TarArchiveOutputStream taos = new TarArchiveOutputStream(gcos);
-			) {
+		for(String filename : fileSet) {
+			String tarGzFileName = filename + "-" + dateSuffix + ".tar.gz";
+			
+			Path logDir = Paths.get(defaultPath);
+
 			Files.list(logDir).filter(Files::isRegularFile)
-					.filter(file->file.getFileName().toString().contains(".log"))
-					.forEach(path -> {
-
-				try {
-					addFileToTarGz(taos, path.toFile(), ".");
-				} catch (IOException e) {
-					System.err.println("Error adding file to tar.gz: " + path);
-				}
-
-
+				.filter(file->file.getFileName().toString().contains(".log"))
+				.forEach(path -> {
+					try (FileOutputStream fos = new FileOutputStream(tarGzFileName);
+						 GzipCompressorOutputStream gcos = new GzipCompressorOutputStream(fos);
+						 TarArchiveOutputStream taos = new TarArchiveOutputStream(gcos);
+					) {
+						File file = path.toFile();
+						addFileToTarGz(taos, file, ".");
+						file.delete();
+						stringBuilder.append("Log Files have been compressed from " + file.getName() + "\n");
+						stringBuilder.append("Log Files have been compressed to " + tarGzFileName + "\n");
+					} catch (IOException e) {
+						stringBuilder.append("Error adding file to tar.gz: " + path + "\n");
+					}
 			});
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 
-		System.out.println("Log Files have been compressed to " + tarGzFileName);
+		stringBuilder.append("Schedule End\n");
+		System.out.println(stringBuilder);
 	}
 
 	private static void addFileToTarGz(TarArchiveOutputStream taos, File file, String base) throws IOException {
